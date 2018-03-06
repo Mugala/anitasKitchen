0fedb000fec84b0dd2ddd8839dc51a430964dc8a
mugala <mugalalevi21@gmail.com>
2018-03-06 22:02:04 +0300

    Hamburger menu This commit will add the hambuger menu

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function() {
+    $("#open-menu").click(function() {
+      $("aside").animate({
+        "left": 0
+      }, 2500);
+    });
+    $("#close-menu").click(function() {
+      $("aside").animate({
+        "left": "-100\%"
+      }, 2500);
+    });
+  
+  });
